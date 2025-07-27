f945e7c552e812b65ab1ed003d1476b3a012ef36
David Bradshaw <dabrad26@me.com>
2025-07-26 21:30:05 -0700

    fix: alter DB to just have pet and not divide tables

---
+++ pet.php
@@ -0,0 +1,154 @@
+<?php include 'helpers.php'; ?>
+
+<?php
+$method = get_method();
+$data = get_request_data();
+
+function get_single_item($id)
+{
+  $db_results = query_database("SELECT * FROM Pet WHERE ID = $id");
+  $final_results = array();
+
+  if (mysqli_num_rows($db_results) > 0) {
+    while ($row = mysqli_fetch_assoc($db_results)) {
+      $id = $row["id"];
+      $chat_db_results = query_database("SELECT * FROM Chat WHERE post_id = $id ORDER BY created_at DESC");
+      $chats = array();
+
+      if (mysqli_num_rows($chat_db_results) > 0) {
+        while ($row = mysqli_fetch_assoc($chat_db_results)) {
+          array_push($chats, $row);
+        }
+      }
+
+      $row["reunited"] = $row["reunited"] === "1" ? true : false;
+      $row["chats"] = $chats;
+      $row["images"] = explode("~", $row["image_data"]);
+      unset($row["image_data"]);
+
+      array_push($final_results, $row);
+    }
+  }
+
+  send_response([
+    'status' => 'success',
+    'data' => $final_results[0],
+  ]);
+}
+
+if ($method === 'GET') {
+  if (get_query_string("id")) {
+    get_single_item(get_query_string("id"));
+
+    return;
+  }
+
+  $db_results = query_database("SELECT * FROM Pet ORDER BY created_at DESC");
+  $final_results = array();
+
+  if (mysqli_num_rows($db_results) > 0) {
+    while ($row = mysqli_fetch_assoc($db_results)) {
+      $id = $row["id"];
+      $chat_db_results = query_database("SELECT * FROM Chat WHERE  post_id = $id ORDER BY created_at DESC");
+      $chats = array();
+
+      if (mysqli_num_rows($chat_db_results) > 0) {
+        while ($row = mysqli_fetch_assoc($chat_db_results)) {
+          array_push($chats, $row);
+        }
+      }
+
+      $row["reunited"] = $row["reunited"] === "1" ? true : false;
+      $row["chats"] = $chats;
+      $row["images"] = explode("~", $row["image_data"]);
+      unset($row["image_data"]);
+
+      array_push($final_results, $row);
+    }
+  }
+
+  send_response([
+    'status' => 'success',
+    'data' => $final_results,
+  ]);
+}
+
+if ($method === 'POST') {
+  $all_keys = array();
+  $all_values = array();
+
+  foreach ($data as $key => $value) {
+    $final_value = $value;
+
+    if ($key === "images") {
+      if (is_array($final_value)) {
+        array_push($all_keys, "image_data");
+        $final_value = implode("~", $final_value);
+        array_push($all_values, "\"$final_value\"");
+      }
+    } else {
+      if (gettype($value) === 'string') {
+        $final_value = "\"$value\"";
+      }
+
+      array_push($all_keys, $key);
+      array_push($all_values, $final_value);
+    }
+  }
+
+  $keys = implode(", ", $all_keys);
+  $values = implode(", ", $all_values);
+
+  $new_id = query_database("INSERT INTO Pet ($keys) VALUES ($values)", true);
+
+  get_single_item($new_id);
+}
+
+if ($method === 'PUT') {
+  $id = get_query_string("id");
+
+  if (!$id) {
+    send_response(array(
+      'code' => 422,
+      'data' => 'Must set id via query param'
+    ), 422);
+
+    return;
+  }
+
+  if (array_key_exists('reunited', $data)) {
+    $reunited_value = $data["reunited"] ? "true" : "false";
+
+    query_database("UPDATE Pet SET reunited = $reunited_value WHERE id = $id;");
+
+    send_response([
+    'status' => 'success',
+  ]);
+  } else {
+    send_response(array(
+      'code' => 422,
+      'data' => 'No valid update call'
+    ), 422);
+
+    return;
+  }
+}
+
+if ($method === 'DELETE') {
+  $id = get_query_string("id");
+
+  if (!$id) {
+    send_response(array(
+      'code' => 422,
+      'data' => 'Must set id via query param'
+    ), 422);
+
+    return;
+  }
+
+  query_database("DELETE FROM Pet WHERE id=\"$id\"");
+
+  send_response([
+    'status' => 'success',
+  ]);
+}
--- seed.sql
+++ seed.sql
@@ -2,40 +2,26 @@
 
 USE PawsitiveID;
 
-CREATE TABLE FoundPet (
+CREATE TABLE Pet (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
+    post_type ENUM('FOUND', 'LOST'),
     animal_type ENUM('CAT', 'DOG', 'RABBIT', 'BIRD', 'OTHER'),
     description VARCHAR(6500),
     last_seen_date DATE,
     last_seen_long DECIMAL(9,6) ,
     last_seen_lat DECIMAL(9,6) ,
-    found_by_name VARCHAR(255),
-    found_by_phone VARCHAR(255),
-    found_by_email VARCHAR(255),
+    post_by_name VARCHAR(255),
+    post_by_phone VARCHAR(255),
+    post_by_email VARCHAR(255),
     created_at TIMESTAMP DEFAULT now(),
-    photo MEDIUMBLOB
-);
-
-CREATE TABLE LostPet (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255),
-    animal_type ENUM('CAT', 'DOG', 'RABBIT', 'BIRD', 'OTHER'),
-    description VARCHAR(6500),
-    last_seen_date DATE,
-    last_seen_long DECIMAL(9,6) ,
-    last_seen_lat DECIMAL(9,6) ,
-    owner_name VARCHAR(255),
-    owner_phone VARCHAR(255),
-    owner_email VARCHAR(255),
-    created_at TIMESTAMP DEFAULT now(),
-    photo MEDIUMBLOB
+    reunited BOOLEAN DEFAULT false,
+    image_data LONGBLOB NOT NULL
 );
 
 CREATE TABLE Chat (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     message VARCHAR(65000),
-    type ENUM('FOUND', 'LOST'),
     post_id INT,
     created_at TIMESTAMP DEFAULT now()
 )
